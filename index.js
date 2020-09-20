439509c6638e0c98e81e3810d635bf035b326483
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-20 20:52:02 +0200

    Added Styling Hero and Header

---
+++ index.js
@@ -0,0 +1,15 @@
+// function to select elements
+
+const selectElement = (s) => document.querySelector(s);
+
+//open the menu on click
+
+selectElement(`.open`).addEventListener("click", () => {
+  selectElement(`.nav-list`).classList.add("active");
+});
+
+//close menu on click
+
+selectElement(`.close`).addEventListener("click", () => {
+  selectElement(`.nav-list`).classList.remove("active");
+});
